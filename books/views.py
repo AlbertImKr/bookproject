86e838e57896d34cdf467b72ee8bff8bb906da8a
ALbertIM0427 <guang7226961@naver.com>
2024-08-12 12:49:08 +0900

    feat: 세션 4- CreateView를 활용한 도서 생성 기능 구현

--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
+from .forms import BookForm
 from .models import Book
 
 
@@ -9,9 +12,18 @@ class BookListView(ListView):
     template_name = 'books/book_list.html'
     context_object_name = 'books'
     ordering = ['-publication_date']
+    paginate_by = 1
 
 
 class BookDetailView(DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
+    success_url = reverse_lazy('books:book_list')
+
+
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('books:book_list')
