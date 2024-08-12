a9c7f89088b18b996146a84f007062292418fc7e
ALbertIM0427 <guang7226961@naver.com>
2024-08-12 12:19:09 +0900

    feat: 세션 3- DetailView를 활용한 도서 상세 페이지 구현

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Book
 
@@ -8,3 +9,9 @@ class BookListView(ListView):
     template_name = 'books/book_list.html'
     context_object_name = 'books'
     ordering = ['-publication_date']
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
